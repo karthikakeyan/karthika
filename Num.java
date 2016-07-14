6168eef8cc863a0d8302b7502dd1bbc250b938bf
karthikakeyan <karthikakeyan@users.noreply.github.com>
2016-07-14 14:58:04 +0530

    Update and rename README.md to Num.java

--- Num.java
+++ Num.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 {
 int n;
 Scanner oe=new Scanner(System.in);
-no=oe.nextInt();
+n=oe.nextInt();
 if(n % 2 == 0)
 System.out.println("It is Even  number");
 else
